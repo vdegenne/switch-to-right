c320bb8433daf0c70502196612d2ae224144746d
Valentin Degenne <vdegenne@gmail.com>
2024-12-08 15:17:16 +0100

    no triggers

--- src/gamepad.ts
+++ src/gamepad.ts
@@ -3,11 +3,18 @@ import {callback, store} from './store.js';
 
 gameControl.on('connect', (gamepad) => {
 	let leftTriggerOn = false;
+	let rightTriggerOn = false;
 	gamepad.before(XBoxButton.LEFT_TRIGGER, () => {
 		leftTriggerOn = true;
 	});
 	gamepad.after(XBoxButton.LEFT_TRIGGER, () => {
 		leftTriggerOn = false;
+	});
+	gamepad.before(XBoxButton.RIGHT_TRIGGER, () => {
+		rightTriggerOn = true;
+	});
+	gamepad.after(XBoxButton.RIGHT_TRIGGER, () => {
+		rightTriggerOn = false;
 	});
 	gamepad.before(XBoxButton.B, () => {
 		if (document.hasFocus()) {
@@ -19,7 +26,9 @@ gameControl.on('connect', (gamepad) => {
 		}
 	});
 	gamepad.before(XBoxButton.Y, () => {
-		store.activated = true;
-		callback();
+		if (!leftTriggerOn && !rightTriggerOn) {
+			store.activated = true;
+			callback();
+		}
 	});
 });
