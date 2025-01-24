5b1ccfe884555770e325150b99cece93537581a7
Valentin Degenne <vdegenne@gmail.com>
2025-01-24 15:22:24 +0100

    don't you ever like console.log

--- src/styles/shared.ts
+++ src/styles/shared.ts
@@ -2,8 +2,6 @@ import {setBaseStyles} from 'lit-with-styles';
 import globals from './stylesheets/globals.css?inline';
 import sharedStyles from './stylesheets/shared.css?inline';
 
-console.log(sharedStyles);
-
 setBaseStyles(sharedStyles);
 
 const fontsStyleSheet = new CSSStyleSheet();
